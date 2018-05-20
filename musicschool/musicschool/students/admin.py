419aaca95d854f6cbdf29042d82ed7a7a00c90a3
issey jordan <issey.jordan@connect.qut.edu.au>
2018-05-20 15:42:19 +1000

    add user via User class API

--- musicschool/musicschool/students/admin.py
+++ musicschool/musicschool/students/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Students
 
 class StudentsAdmin(admin.ModelAdmin):
-    list_display = ('first_name',)
+    list_display = ('id','first_name','last_name', 'address','date_of_birth','phone_number', 'email_address', 'facebook')
 
 admin.site.register(Students, StudentsAdmin)
